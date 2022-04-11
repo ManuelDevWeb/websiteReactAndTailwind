911135b4e12a00c9decb2e7c03b267ed65ed2c0a
ManuelDev <manueldeveloper17@gmail.com>
2022-04-10 21:19:19 -0500

    Create component Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // Importando Navbar
 import Navbar from './components/Navbar';
+// Importando Hero
+import Hero from './components/Hero';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,62 @@
+// Importando imágenes
+import bgImg from '../assets/cyber-bg.png';
+
+// Importando iconos de Heroicons
+import {
+  CloudUploadIcon,
+  DatabaseIcon,
+  PaperAirplaneIcon,
+  ServerIcon,
+} from '@heroicons/react/outline';
+
+const Hero = () => {
+  return (
+    <div className="w-full h-screen bg-zinc-200 flex flex-col justify-between">
+      <div className="grid md:grid-cols-2 max-w-[1240px] m-auto">
+        {/* Información */}
+        <div className="flex flex-col justify-center md:items-start w-full px-2 py-8">
+          <p className="text-2xl">Unique Sequencing & Production</p>
+          <h1 className="py-3 text-4xl md:text-7xl font-bold">
+            Cloud Management
+          </h1>
+          <p className="text-2xl">This is our Tech brand.</p>
+          <button className="py-3 sm:w-[60%] my-4">Get Started</button>
+        </div>
+
+        {/* Imagen */}
+        <div>
+          <img src={bgImg} alt="Cyber" className="w-full" />
+        </div>
+
+        {/* Cards */}
+        <div
+          className="absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%] 
+          mx-1 md:mx-0 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 border 
+          border-slate-300 rounded-xl text-center shadow-xl"
+        >
+          <p>Data Services</p>
+          <div className="flex justify-between flex-wrap px-4">
+            <p className="flex px-4 py-2 text-slate-500">
+              <CloudUploadIcon className="w-6 text-indigo-600" />
+              App Security
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <DatabaseIcon className="w-6 text-indigo-600 " />
+              Dashboard Design
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <ServerIcon className="w-6 text-indigo-600" />
+              Cloud Data
+            </p>
+            <p className="flex px-4 py-2 text-slate-500">
+              <PaperAirplaneIcon className="w-6 text-indigo-600" />
+              API
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
